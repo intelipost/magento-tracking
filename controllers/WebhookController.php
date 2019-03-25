4ea6bf71483c6575bf73ad11d17cb5616714915a
rms <sm.rogerio@hotmail.com>
2019-03-25 14:02:40 -0300

    Fix webhook receiver

--- controllers/WebhookController.php
+++ controllers/WebhookController.php
@@ -2,154 +2,138 @@
 
 class Intelipost_Tracking_WebhookController extends Mage_Core_Controller_Front_Action
 {
-	public function indexAction()
-	{
-		$start = microtime();
+    public function indexAction()
+    {
+        $start = microtime();
 
-        if ($this->getRequest()->isPost() && $webhookActive)
-		{
-			if ($this->getRequest()->getHeader('api-key') == Mage::helper('basic')->getDecriptedKey('apikey'))
-			{
-				$pre_dispatch_events = array('NEW', 'READY_FOR_SHIPPING', 'SHIPPED');
-				$post_dispatch_events = array('TO_BE_DELIVERED', 'IN_TRANSIT', 'DELIVERED', 'CLARIFY_DELIVERY_FAIL', 'DELIVERY_FAILED');
+        $webhookActive = Mage::getStoreConfig("intelipost_tracking/webhook/active");
+        $trackingPreDispatch = Mage::getStoreConfig("intelipost_tracking/webhook/tacking_pre_dispatch");
+        $trackingPostDispatch = Mage::getStoreConfig("intelipost_tracking/webhook/tacking_post_dispatch");
 
-				$value = json_decode($this->getRequest()->getRawBody());								
-				$order_id = $value->order_number;
+        if ($webhookActive) {
 
-				if (Mage::getStoreConfig('intelipost_tracking/webhook/format_order_prefix'))
-				{
-					if (strpos(Mage::getStoreConfig('intelipost_tracking/webhook/order_prefix'), ',') !== false)
-					{
-						$result = explode(',', Mage::getStoreConfig('intelipost_tracking/webhook/order_prefix'));
+            if ($this->getRequest()->getHeader('api-key') == Mage::helper('basic')->getDecriptedKey('apikey')) {
 
-						foreach ($result as $r) 
-						{
-							$order_id = trim(str_replace($r, '', $order_id));
-						}
-					}
-					else
-					{
-						$order_id = trim(str_replace(Mage::getStoreConfig('intelipost_tracking/webhook/order_prefix'), '', $order_id));
-					}
-				}
+                $pre_dispatch_events = array('NEW', 'READY_FOR_SHIPPING', 'SHIPPED');
+                $post_dispatch_events = array('TO_BE_DELIVERED', 'IN_TRANSIT', 'DELIVERED', 'CLARIFY_DELIVERY_FAIL', 'DELIVERY_FAILED');
 
-				if (Mage::getStoreConfig('intelipost_tracking/webhook/format_order_sufix'))
-				{
-					if (strpos(Mage::getStoreConfig('intelipost_tracking/webhook/order_sufix'), ',') !== false)
-					{
-						$result = explode(',', Mage::getStoreConfig('intelipost_tracking/webhook/order_sufix'));
+                $value = json_decode($this->getRequest()->getRawBody());
+                $order_id = $value->order_number;
 
-						foreach ($result as $r) 
-						{
-							$order_id = trim(str_replace($r, '', $order_id));
-						}
-					}
-					else
-					{
-						$order_id = trim(str_replace(Mage::getStoreConfig('intelipost_tracking/webhook/order_sufix'), '', $order_id));
-					}
-				}
+                if (Mage::getStoreConfig('intelipost_tracking/webhook/format_order_prefix')) {
+                    if (strpos(Mage::getStoreConfig('intelipost_tracking/webhook/order_prefix'), ',') !== false) {
+                        $result = explode(',', Mage::getStoreConfig('intelipost_tracking/webhook/order_prefix'));
 
-				$order = Mage::getModel('sales/order')->load($order_id, 'increment_id');
+                        foreach ($result as $r) {
+                            $order_id = trim(str_replace($r, '', $order_id));
+                        }
+                    } else {
+                        $order_id = trim(str_replace(Mage::getStoreConfig('intelipost_tracking/webhook/order_prefix'), '', $order_id));
+                    }
+                }
 
-				$state = $value->history->shipment_order_volume_state;
+                if (Mage::getStoreConfig('intelipost_tracking/webhook/format_order_sufix')) {
+                    if (strpos(Mage::getStoreConfig('intelipost_tracking/webhook/order_sufix'), ',') !== false) {
+                        $result = explode(',', Mage::getStoreConfig('intelipost_tracking/webhook/order_sufix'));
 
-				if ((in_array($state, $pre_dispatch_events) && $tracking_pre_dispatch)
-					|| in_array($state, $post_dispatch_events) && $tracking_post_dispatch)
-				{								
+                        foreach ($result as $r) {
+                            $order_id = trim(str_replace($r, '', $order_id));
+                        }
+                    } else {
+                        $order_id = trim(str_replace(Mage::getStoreConfig('intelipost_tracking/webhook/order_sufix'), '', $order_id));
+                    }
+                }
 
-					switch (strtoupper($state))
-					{
-						case 'NEW':
-							$status = Mage::getStoreConfig ("intelipost_tracking/webhook/new");
-							$order->setStatus ($status);
-							break;					
+                $order = Mage::getModel('sales/order')->load($order_id, 'increment_id');
 
-						case 'READY_FOR_SHIPPING':
-							$status = Mage::getStoreConfig ("intelipost_tracking/webhook/ready_for_shipping");
-							$order->setStatus ($status);
-							break;
+                $state = $value->history->shipment_order_volume_state;
 
-						case 'SHIPPED':
-							$status = Mage::getStoreConfig ("intelipost_tracking/webhook/shipped");
-							$order->setStatus ($status);
-							break;
+                if ((in_array($state, $pre_dispatch_events) && $trackingPreDispatch)
+                    || in_array($state, $post_dispatch_events) && $trackingPostDispatch) {
+                    Mage::log("Status allowed");
 
-						case 'TO_BE_DELIVERED':
-							$status = Mage::getStoreConfig ("intelipost_tracking/webhook/to_be_delivered");
-							$order->setStatus ($status);
-							break;
-						
-						case 'IN_TRANSIT':
-							$status = Mage::getStoreConfig ("intelipost_tracking/webhook/in_transit");
-							$order->setStatus ($status);
-							break;
+                    switch (strtoupper($state)) {
+                        case 'NEW':
+                            $status = Mage::getStoreConfig("intelipost_tracking/webhook/new");
+                            $order->setStatus($status);
+                            break;
 
-						case 'DELIVERED':
-							$status = Mage::getStoreConfig ("intelipost_tracking/webhook/delivered");
-							$order->setStatus ($status);
-							break;
+                        case 'READY_FOR_SHIPPING':
+                            $status = Mage::getStoreConfig("intelipost_tracking/webhook/ready_for_shipping");
+                            $order->setStatus($status);
+                            break;
 
-						case 'CLARIFY_DELIVERY_FAIL':
-							$status = Mage::getStoreConfig ("intelipost_tracking/webhook/delivery_late");
-							$order->setStatus ($status);
-							break;
+                        case 'SHIPPED':
+                            $status = Mage::getStoreConfig("intelipost_tracking/webhook/shipped");
+                            $order->setStatus($status);
+                            break;
 
-						case 'DELIVERY_FAILED':
-							$status = Mage::getStoreConfig ("intelipost_tracking/webhook/delivery_failed");
-							$order->setStatus ($status);
-							break;
-					}
+                        case 'TO_BE_DELIVERED':
+                            $status = Mage::getStoreConfig("intelipost_tracking/webhook/to_be_delivered");
+                            $order->setStatus($status);
+                            break;
 
-					if (Mage::getStoreConfig('intelipost_tracking/webhook/save_comments'))
-					{
-						$comments_options = '';
-						$admin_ident_prefix = '[Intelipost Webhook] - ';
-						$comment = $value->history->provider_message;
-						$order->addStatusHistoryComment($admin_ident_prefix . $comment);
+                        case 'IN_TRANSIT':
+                            $status = Mage::getStoreConfig("intelipost_tracking/webhook/in_transit");
+                            $order->setStatus($status);
+                            break;
 
-						if (Mage::getStoreConfig('intelipost_tracking/webhook/show_frontend_comments'))
-						{
-							$comments_options = Mage::getStoreConfig('intelipost_tracking/webhook/frontend_comment_options');
-							if (strpos($comments_options, ',') !== false) {
-								$comments_options = explode(',', $comments_options);
-							}
-						}
+                        case 'DELIVERED':
+                            $status = Mage::getStoreConfig("intelipost_tracking/webhook/delivered");
+                            $order->setStatus($status);
+                            break;
 
-						if (is_array($comments_options))
-						{						
-							foreach($order->getShipmentsCollection() as $shipment)
-        					{
-        						$shipment->addComment($comment, false, true);
-        						$shipment->save();
-        					}
+                        case 'CLARIFY_DELIVERY_FAIL':
+                            $status = Mage::getStoreConfig("intelipost_tracking/webhook/delivery_late");
+                            $order->setStatus($status);
+                            break;
 
-							$order->addStatusHistoryComment($comment)->setIsVisibleOnFront(1);
-						}
-						else
-						{
-							if ($comments_options == 'order')
-							{
-								$order->addStatusHistoryComment($comment)->setIsVisibleOnFront(1);
-							}
-							else
-							{
-								foreach($order->getShipmentsCollection() as $shipment)
-	        					{
-	        						$shipment->addComment($comment, false, true);
-	        						$shipment->save();
-	        					}
-							}
-						}
-					}
+                        case 'DELIVERY_FAILED':
+                            $status = Mage::getStoreConfig("intelipost_tracking/webhook/delivery_failed");
+                            $order->setStatus($status);
+                            break;
+                    }
 
-					$order->save();			
-				}		
-			}
-		}   
+                    if (Mage::getStoreConfig('intelipost_tracking/webhook/save_comments')) {
 
-		$time_elapsed_secs = microtime() - $start;
+                        $comments_options = '';
+                        $admin_ident_prefix = '[Intelipost Webhook] - ';
+                        $comment = $value->history->provider_message;
+                        $order->addStatusHistoryComment($admin_ident_prefix . $comment);
 
-		printf('Tempo Processamento: %.5f sec',  $time_elapsed_secs);   
-	}
+                        if (Mage::getStoreConfig('intelipost_tracking/webhook/show_frontend_comments')) {
+                            $comments_options = Mage::getStoreConfig('intelipost_tracking/webhook/frontend_comment_options');
+                            if (strpos($comments_options, ',') !== false) {
+                                $comments_options = explode(',', $comments_options);
+                            }
+                        }
+
+                        if (is_array($comments_options)) {
+                            foreach ($order->getShipmentsCollection() as $shipment) {
+                                $shipment->addComment($comment, false, true);
+                                $shipment->save();
+                            }
+
+                            $order->addStatusHistoryComment($comment)->setIsVisibleOnFront(1);
+                        } else {
+                            if ($comments_options == 'order') {
+                                $order->addStatusHistoryComment($comment)->setIsVisibleOnFront(1);
+                            } else {
+                                foreach ($order->getShipmentsCollection() as $shipment) {
+                                    $shipment->addComment($comment, false, true);
+                                    $shipment->save();
+                                }
+                            }
+                        }
+                    }
+                    echo "order->save" . PHP_EOL;
+                    $order->save();
+                }
+            }
+        }
+
+        $time_elapsed_secs = microtime() - $start;
+
+        printf('Tempo Processamento: %.5f sec', $time_elapsed_secs);
+    }
 }
